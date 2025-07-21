9c60d2a4be2997d1b8dd11c7f6fee26f75bb78a5
da-moon <github.com.3qrsg@passmail.net>
2025-07-21 11:03:57 -0400

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://git.io/termux | bash -s -- --zsh --python --neovim
+
+# NOTE: nerdfonts
+
+curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
