a523806de6f0aabe15cad46b338ca7ef2f055586
da-moon <github.com.3qrsg@passmail.net>
2025-08-08 05:19:44 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+set -efuo pipefail
 
-curl -fsSL https://git.io/termux | bash -s -- --zsh --python --neovim
-
+if [ ! -r "~/.zshrc" ] ;
+  curl -fsSL https://git.io/termux | bash -s -- --zsh --python --neovim ;
+fi
 # NOTE: nerdfonts
-
 curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
+
+pkg install -y \
+  git-delta \
+  difftastic ;
