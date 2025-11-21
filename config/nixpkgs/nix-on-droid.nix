d5b5273164c0a757ed40b8fb36f08b7d1f1c7459
da-moon <github.com.3qrsg@passmail.net>
2025-11-21 11:57:53 -0500

    Add Nix configuration for nix-on-droid

---
+++ config/nixpkgs/nix-on-droid.nix
@@ -0,0 +1,74 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    # User-facing stuff that you really really want to have
+    vim
+    git
+    wget
+    jq
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    # man is now managed by home-manager
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+    openssh
+    eza
+    fzf
+    rustup
+    # Managed by home-manager (see home.nix):
+    # nixfmt-rfc-style, helix, fd, ripgrep, zellij, delta, starship, sd, zoxide
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Set default SHELL environment variable
+  environment.sessionVariables = {
+    SHELL = "${pkgs.bash}/bin/bash";
+  };
+
+  # Set your time zone
+  #time.timeZone = "Europe/Berlin";
+
+  # Home-manager configuration (modular setup)
+  # Configuration is organized in:
+  # - home.nix (main file with imports)
+  # - programs/*.nix (per-program configs)
+  # - packages.nix (package list)
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+
+    # Import the modular home.nix
+    config = import ./home.nix;
+  };
+}
+
+# vim: ft=nix
