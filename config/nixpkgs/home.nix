9afdbea17913eff2d433b6ea0cc5693d0cd18a15
da-moon <github.com.3qrsg@passmail.net>
2025-11-21 12:12:03 -0500

    Add git configuration to home.nix imports

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -1,9 +1,15 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   # Import modular configurations
   imports = [
     ./programs/bash.nix
+    ./programs/git.nix
     ./programs/starship.nix
     ./programs/zoxide.nix
     ./programs/man.nix
